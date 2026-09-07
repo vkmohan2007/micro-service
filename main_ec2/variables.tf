39533a0c79ea9d52aeb9f157239da015e1fd464d
vkmohan2007 <vkmohan2007@gmail.com>
2026-09-08 00:41:04 +0530

    Update variables.tf

--- main_ec2/variables.tf
+++ main_ec2/variables.tf
@@ -64,13 +64,13 @@ variable "iam-role" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-07a00cf47dbbc844c" // Replace with the latest AMI ID for your region
+  default     = "ami-01a00762f46d584a1" // Replace with the latest AMI ID for your region
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "m7i-flex.large"
+  default     = "t2.medium"
 }
 
 variable "key_name" {
